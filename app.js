9835cca9ffc1dfc15eb18474c0e4d69581c5dac0
raihanrehan <raihangoceng08@gmail.com>
2024-11-06 23:03:45 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+  const question = faq.querySelector(".question");
+  const answer = faq.querySelector(".answer");
+
+  question.addEventListener("click", () => {
+    if (faq.classList.contains("active")) {
+      faq.classList.remove("active");
+      answer.style.maxHeight = null;
+    } else {
+      // закрытие других активных блоков
+      faqs.forEach(otherFaq => {
+        if (otherFaq.classList.contains("active")) {
+          otherFaq.classList.remove("active");
+          otherFaq.querySelector(".answer").style.maxHeight = null;
+        }
+      });
+
+      faq.classList.add("active");
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+});
